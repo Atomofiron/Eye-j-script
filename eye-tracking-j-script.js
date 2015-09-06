c923fd2840b0b85dcdd4e726cccf9044c5f1572e
Ярослав Нестеров <atomofiron@yandex.ru>
2015-09-06 14:51:27 +0300

    Update eye-tracking-j-script.js

--- eye-tracking-j-script.js
+++ eye-tracking-j-script.js
@@ -1,13 +1,15 @@
 function getCfg(s) {
 	switch (s) {
-		case "Hscope": return 6; // горизонтальный размах
-		case "Vscope": return 8; // вертикальный размах
+		case "Hscope": return 50; // горизонтальный размах
+		case "Vscope": return 50; // вертикальный размах
 		case "dotHeight": return 30; // высота зрачка
 		case "eyes": return 2; // кол-во глаз
 		case "dotImg": return "dot.png"; // путь к зрачоку
 		case "ones": return true; // одна точка зрения
-		case "bg": return true; // фон зрачка
-		case "bgColor": return "#eeb89c"; // цвет фона зрачка
+		case "bg": return false; // фон зрачка
+		case "bgColor": return "#00ff00"; // цвет фона зрачка
+		case "len": return true; // большая точка фокуса
+		case "lenInt": return 200; // размер фокуса
 		default: return 0;
 	}
 }
@@ -55,8 +57,10 @@ function go(x,y,eye,dot) {
 	var sqrT = Math.sqrt(difX*difX+difY*difY);
 	var sinus = difX/sqrT;
 	var cosinus = difY/sqrT;
-	Dot.style.marginLeft = getCfg("Hscope")*sinus;
-	Dot.style.marginTop = getCfg("Vscope")*cosinus;
+	var len = 1;
+	if (sqrT<getCfg("lenInt") && getCfg("len")) len = sqrT/getCfg("lenInt");
+	Dot.style.marginLeft = getCfg("Hscope")*sinus*len;
+	Dot.style.marginTop = getCfg("Vscope")*cosinus*len;
 }
 // Mouse:
 function init() {
