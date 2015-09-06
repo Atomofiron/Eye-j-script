81450ae4fb991c74e709b44c40ace767813aefa5
Ярослав Нестеров <atomofiron@yandex.ru>
2015-09-06 20:24:37 +0300

    Update eye-tracking-j-script.js

--- eye-tracking-j-script.js
+++ eye-tracking-j-script.js
@@ -1,30 +1,15 @@
-function getCfg(s) {
-	switch (s) {
-		case "Hscope": return 50; // горизонтальный размах
-		case "Vscope": return 50; // вертикальный размах
-		case "dotHeight": return 30; // высота зрачка
-		case "eyes": return 2; // кол-во глаз
-		case "dotImg": return "dot.png"; // путь к зрачоку
-		case "ones": return true; // одна точка зрения
-		case "bg": return false; // фон зрачка
-		case "bgColor": return "#00ff00"; // цвет фона зрачка
-		case "len": return true; // большая точка фокуса
-		case "lenInt": return 200; // размер фокуса
-		default: return 0;
-	}
-}
-
+// Добавление элементов на страницу:
 for (var i = 1; i<=getCfg("eyes"); i++) {
 	var Eye = document.getElementById("eye"+i);
 	Eye.style.width = getCfg("dotHeight");
 	Eye.style.height = getCfg("dotHeight");
 	Eye.innerHTML = "<img id='dot" + i + "' src='"+getCfg("dotImg")+"'>";
-	
 	if (getCfg("bg")) Eye.style.background = getCfg("bgColor");
-	
 	document.getElementById("dot"+i).style.height = getCfg("dotHeight");
 }
 
+
+// Изменение положения зрачков:
 function go(x,y,eye,dot) {
 	var Eye = document.getElementById(eye);
 	var Dot = document.getElementById(dot);
@@ -62,7 +47,8 @@ function go(x,y,eye,dot) {
 	Dot.style.marginLeft = getCfg("Hscope")*sinus*len;
 	Dot.style.marginTop = getCfg("Vscope")*cosinus*len;
 }
-// Mouse:
+
+// Отслеживание курсора мыши:
 function init() {
     if (document.layers) document.captureEvents(Event.MOUSEMOVE);
     document.onmousemove=mousemove;
